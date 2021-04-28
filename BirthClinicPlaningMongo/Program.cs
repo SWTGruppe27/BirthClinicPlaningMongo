d9a80c6c761ac3b61e1ea4735deea1338d6adc03
Nugget98 <61585323+Nugget98@users.noreply.github.com>
2021-04-28 10:57:04 +0200

    Add project files.

---
+++ BirthClinicPlaningMongo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BirthClinicPlaningMongo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
